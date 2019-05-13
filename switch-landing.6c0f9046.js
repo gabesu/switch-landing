8347c2191a3d14b227a0d15a8c0c1ad4bd7a8704
Gabriel Esu <mgabrielesu@gmail.com>
2019-05-13 14:10:20 +0100

    Updates

--- switch-landing.6c0f9046.js
+++ switch-landing.6c0f9046.js
@@ -4,4 +4,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";var e=t(require("@glidejs/glide"));function t(e){return e&&e.__esModule?e:{default:e}}new e.default(".glide-two").mount();
 },{"@glidejs/glide":"grrK"}]},{},["Focm"], null)
-//# sourceMappingURL=switch-landing.ec70824a.js.map
+//# sourceMappingURL=switch-landing.6c0f9046.js.map
