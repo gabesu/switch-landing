1896fa1dcdcb777316bf5327a662377d85d10649
Gabriel Esu <mgabrielesu@gmail.com>
2019-05-15 07:08:44 +0100

    Changes to landing

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-// import "./js/cursor";
-// import "./js/animations/account";
-// import "./js/animations/hero";
-// import "./js/animations/why";
-
 import Glide from "@glidejs/glide";
-new Glide(".glide-two").mount();
+new Glide(".glide").mount();
+
+let nav = document.querySelector(".navigation");
+let logo = document.querySelector(".logo");
+let logoAlt = document.querySelector(".logo-alt");
+document.addEventListener("scroll", () => {
+  if (document.documentElement.scrollTop > 0) {
+    console.log("Scrolled");
+    nav.classList.add("scrolled");
+    logo.classList.remove("show");
+    logoAlt.classList.add("show");
+  } else {
+    console.log("home");
+    nav.classList.remove("scrolled");
+    logoAlt.classList.remove("show");
+    logo.classList.add("show");
+  }
+});
